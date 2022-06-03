548463c3a1f1eedb1fef6941efa0ca3469aef1ff
Arthur Silva <>
2022-06-03 20:48:56 +0000

    class 'Disciplina' was created along with its initial setters and getters methods

---
+++ Disciplina.java
@@ -0,0 +1,23 @@
+public class Disciplina {
+  // atributos devem ser privados
+  private String nome;
+  private String codigo;
+  private String duvidas[];
+
+  // definindo os setters e getters dos atributos
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public String getNome() {
+    return nome;
+  }
+
+  public void setCodigo(String codigo) {
+    this.codigo = codigo;
+  }
+  public String getCodigo() {
+    return codigo;
+  }
+
+  
+}
